5f9b91f7bf616e83d67d7289104d0cdad3c22bc3
Uzo <uzoikejiofor@gmail.com>
2022-07-07 14:07:38 -0400

    Orbit list

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -8,10 +8,7 @@ export class Satellite {
 
 	constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
 		this.name = name;
-		this.type = type;
-		this.launchDate = launchDate;
-		this.orbitType = orbitType;
-		this.operational = operational;
+		
    }
 	
 	isSpaceDebris(): boolean {
